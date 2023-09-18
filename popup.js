58c098ed790b2461f05b511f532aa91d7b20cd5c
Anil Maurya <anil.empi@gmail.com>
2023-09-18 13:37:56 +0530

    add copy as markdown

--- popup.js
+++ popup.js
@@ -1,5 +1,43 @@
 let data;
 
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("textInput").focus();
+
+  window.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      window.close();
+    }
+  });
+  const textArea = document.getElementById("textArea");
+
+  textArea.addEventListener("click", (e) => {
+    console.log(e.target.id);
+    deleteItem(e.target.id);
+  });
+
+  chrome.storage.local.get(["brainDump"], (result) => {
+    data = result.brainDump;
+    render();
+  });
+
+  const copyButton = document.getElementById("copy");
+  const copiedButton = document.getElementById("copied");
+
+  copyButton.addEventListener("click", function () {
+    const markdownList = data.map((task) => `- [ ] ${task}`).join("\n");
+    window.navigator.clipboard
+      .writeText(markdownList)
+      .then(function () {
+        console.log("Text successfully copied to clipboard!");
+        copyButton.style.display = "none";
+        copiedButton.style.display = "block";
+      })
+      .catch(function (err) {
+        console.error("Unable to copy text to clipboard", err);
+      });
+  });
+});
+
 function render() {
   // const itemList = data.map((item) => `<li>${item}</li>`).join("");
   const itemList = data
@@ -30,27 +68,6 @@ function setState(value) {
     render();
   });
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("textInput").focus();
-
-  window.addEventListener("keydown", function (event) {
-    if (event.key === "Escape") {
-      window.close();
-    }
-  });
-  const textArea = document.getElementById("textArea");
-
-  textArea.addEventListener("click", (e) => {
-    console.log(e.target.id);
-    deleteItem(e.target.id);
-  });
-
-  chrome.storage.local.get(["brainDump"], (result) => {
-    data = result.brainDump;
-    render();
-  });
-});
 
 document
   .getElementById("textInput")
